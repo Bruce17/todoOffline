4c579e1b2f328cf2b7c0815e938e66da5d6edfba
T.W.Vance <tv66@kent.ac.uk>
2016-04-07 19:05:08 +0100

    First Commit

---
+++ public/_js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $.get('/api/todos',function(res){
+      $.each(res,function(index,value){
+        $("#todoList").prepend("<div class='todo'><p class='todoTitle'><input "+(value.complete == true ? 'checked=checked' : '')+" type='checkbox' class='todoCheck' data-todoId='"+value._id+"'>"+value.title+"</p><p class='todoDesc'>"+value.description+"</p></div>");
+      });
+    },"json");
+
+
+    $("#todoButton").on("click",function(e){
+      e.preventDefault();
+      $("#todoList").html("");
+      $.post('/api/todos/put',{title:$("#todoTitle").val()},function(res){
+        $.each(res,function(index,value){
+          $("#todoList").prepend("<div class='todo'><p class='todoTitle'><input "+(value.complete == true ? 'checked=checked' : '')+" type='checkbox' class='todoCheck' data-todoId='"+value._id+"'>"+value.title+"</p><p class='todoDesc'>"+value.description+"</p></div>");
+        });
+      });
+    });
+
+    $("body").on("click",".todoCheck",function(){
+      var id = $(this).attr("data-todoId");
+      var c = false;
+      if($(this).is(':checked')){
+        c = true;
+      }
+      $.post("/api/todos/complete",{"id":id,complete:c},function(res){});
+    });
+
+});
---
+++ server.js
@@ -0,0 +1,76 @@
+var express = require('express'),
+    app = express(),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+
+app.use(express.static('public'));
+app.set('view engine','jade');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+/*************** DATABASE ************/
+mongoose.connect('mongodb://api:Applemac1@ds019480.mlab.com:19480/todooffline');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+  /*************** SCHEMA ************/
+
+  var TodoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    complete: Boolean
+  });
+
+  /*************** MODELS ************/
+
+  var Todo = mongoose.model('Todo',TodoSchema);
+
+
+  /*************** API ************/
+
+  app.get('/api/todos',function(req,res){
+    Todo.find(function(err,todos){
+      res.json(todos);
+    });
+  });
+
+  app.post('/api/todos/put',function(req,res){
+
+    // Create a new todo and save.
+    var tmpTodo = new Todo({title:req.body.title,description:"",complete:false});
+    tmpTodo.save();
+
+    // Send back all todos for re rendering
+    Todo.find(function(err,todos){
+      res.json(todos);
+    });
+  });
+
+  app.post('/api/todos/complete',function(req,res){
+    Todo.findOneAndUpdate({_id: req.body.id},{complete:req.body.complete},function(err,doc){
+
+    });
+  });
+
+
+  /*************** BASE URL ************/
+  app.get('/',function(req,res){
+    // Send the base file on request
+    res.sendFile(__dirname+'/public/home.html');
+  });
+
+
+});
+
+
+
+
+
+
+
+app.listen(3000)
